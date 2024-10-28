d6828441eceda160bcdc59e10f5c540944c23903
frey <1007921963@qq.com>
2024-10-28 18:06:02 +0800

    24.10.28

---
+++ LeetCode/746.min-cost-climbing-stairs.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=746 lang=cpp
+ * @lcpr version=30204
+ *
+ * [746] 使用最小花费爬楼梯
+ */
+
+
+// @lcpr-template-start
+using namespace std;
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <queue>
+#include <stack>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+// @lcpr-template-end
+// @lc code=start
+class Solution {
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        
+    }
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [10,15,20]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,100,1,1,1,100,1,1,100,1]\n
+// @lcpr case=end
+
+ */
+
